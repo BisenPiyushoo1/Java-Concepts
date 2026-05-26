e20edbb0c8a4e8ef7c2044ef423f3aef70704df8
BisenPiyushoo1 <piyushbisen628@gmail.com>
2026-05-26 23:43:20 +0530

    Thread class

---
+++ MultiThreading.java
@@ -0,0 +1,23 @@
+public class MultiThreading{
+    public static class MyThread extends Thread{
+        @Override
+        public void run(){
+            for (int i=0;i<5;i++){
+                System.out.println("hello world");
+            }
+
+        }
+
+    }
+    public static void main(String[] args) {
+        MyThread t1=new MyThread();
+        t1.start();
+        for (int i=0;i<5;i++){
+            System.out.println(" happy");
+        }
+
+
+    }
+
+}
+
